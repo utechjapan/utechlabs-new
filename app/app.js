78c261f8a4d59689b2879a5f53deafb3d2b7cd03
Chikara Inohara <chikara.inohara@utechjapan.net>
2024-12-10 08:20:01 -0500

    Auto-commit on Tue Dec 10 08:20:01 AM EST 2024

--- app/app.js
+++ app/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Sample Route
 app.get('/', (req, res) => {
     res.send('Hello from the App Server!');
 });
 
+// API Route Example
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This is your app server data.' });
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`App server running on port ${PORT}`);
 });
